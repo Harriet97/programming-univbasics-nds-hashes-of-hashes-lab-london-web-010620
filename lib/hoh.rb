e9c201c2868ac3caaa4d6cc7f690bea71ac8f682
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-10 14:47:59 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -51,4 +51,16 @@ a_kingdom = { :label => "Kingdom",
                     }
                   }
               }
-}end
+}
+end
+
+
+  #  Kingdom
+  #  Phylum
+  #  Class
+  #  Order
+  #  Family
+  #  Genus
+  #  Species
+  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
+  # HoH all the way to Species!
