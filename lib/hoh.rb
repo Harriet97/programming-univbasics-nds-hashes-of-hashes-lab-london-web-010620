629ebdeed260dc877dacda27cbc4ab2c973618ce
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-10 14:47:03 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -35,13 +35,13 @@ a_kingdom = { :label => "Kingdom",
               :sub_category =>{
                 :label => "Phylum",
                 :sub_category => {
-                  :label=> "Class"
+                  :label=> "Class",
                   :sub_category =>{
-                    :label => "Order"
+                    :label => "Order",
                     :sub_category => {
                       :label => "Family",
                       :sub_category => {
-                        :label => "Genus"
+                        :label => "Genus",
                         :sub_category => {
                           :label => "Species",
                           :sub_category =>   nil
