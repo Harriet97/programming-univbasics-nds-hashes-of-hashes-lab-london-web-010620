565edd738875f664ef0ac1ce3cc169d06deed226
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-10 14:38:45 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,31 +31,32 @@
 # }
 
 def naming_system
-PORTION_1 = {"Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
-  #  Kingdom
+PORTION_1 = { :Kingdom => "", 
+              :sub_category: ==>{
+                :label => "Phylum",
+                :sub_category: {
+                  :label=> "Class"
+                  }
+              }
+}
+
+PORTION_2 = {
+  :label => "Order"
+}
+
+PORTION_3 = {
+  :label => "Family",
+  :sub_category => {
+    :label => "Genus"
+  }
+}
+
+PORTION_4 = {
+   label: "Species",
+   sub_category: nil
+ }
+ 
+  #Kingdom
   #  Phylum
   #  Class
   #  Order
